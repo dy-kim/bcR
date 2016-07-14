bca2f359f1f30df2de2d57d1e864af288d11cc75
Dae Young Kim <daykim22@gmail.com>
2016-07-14 20:50:42 +0900

    Strict check & Add .travis.yml

--- R/convertBase.R
+++ R/convertBase.R
@@ -46,78 +46,79 @@ convertBase <- function(x, inputBase, outputBase) {
   return(.convertBaseVectorized(x, inputBase, outputBase))
 }
 
-
-######################
-#  Applied Functions
-######################
-
-#' Convert hexa value to decimal value
+#' Which numeral bases are convertible
 #'
-#' @export
-convertHex2Dec <- function(hexaValue) {
-  result <-  convertBase(x = hexaValue,
-                         inputBase  = '16',
-                         outputBase = '10')
-  
-  return(result)
-}
-
-#' Convert decimal value to hexa value
-#'
-#' @export
-convertDec2Hex <- function(decValue) {
-  decVal <- assureNumeric(decValue)
-  
-  if (is.null(decVal)) {
-    message('Input value is not numeric. Return NULL.')
-    return(NULL)
-  }
-  
-  result <- convertBase(x = decVal,
-                        inputBase  = '10',
-                        outputBase = '16')
-  
-  return(result)
-}
-
-#' Convert decimal value to binary value
-#'
-#' @export
-convertDec2Bin <- function(decValue) {
-  decVal <- assureNumeric(decValue)
-  
-  if (is.null(decVal)) {
-    message('Input value is not numeric. Return NULL.')
-    return(NULL)
-  }
-  
-  result <- convertBase(x = decVal,
-                        inputBase  = '10',
-                        outputBase = '2')
-  
-  return(result)
-}
-
-#' Convert binary value to decimal value
-#'
-#' @export
-convertBin2Dec <- function(binValue) {
-  binVal <- assureNumeric(binValue)
-  
-  if (is.null(binVal)) {
-    message('Input value is not numeric. Return NULL.')
-    return(NULL)
-  }
-  
-  result <- convertBase(x = bin.val,
-                        inputBase  = '2',
-                        outputBase = '10')
-  
-  return(result)
-}
-
-
 #' @export
 getConvertibleBase <- function() {
   return(c('2', '10', '16'))
 }
+
+#' ######################
+#' #  Applied Functions
+#' ######################
+#' 
+#' #' Convert hexa value to decimal value
+#' #' 
+#' #' 
+#' #' @export
+#' convertHex2Dec <- function(x) {
+#'   result <-  convertBase(x,
+#'                          inputBase  = '16',
+#'                          outputBase = '10')
+#' 
+#'   return(result)
+#' }
+#' 
+#' #' Convert decimal value to hexa value
+#' #'
+#' #' @export
+#' convertDec2Hex <- function(x) {
+#'   decVal <- assureNumeric(x)
+#' 
+#'   if (is.null(decVal)) {
+#'     message('Input value is not numeric. Return NULL.')
+#'     return(NULL)
+#'   }
+#' 
+#'   result <- convertBase(x = decVal,
+#'                         inputBase  = '10',
+#'                         outputBase = '16')
+#' 
+#'   return(result)
+#' }
+#' 
+#' #' Convert decimal value to binary value
+#' #'
+#' #' @export
+#' convertDec2Bin <- function(x) {
+#'   decVal <- assureNumeric(x)
+#' 
+#'   if (is.null(decVal)) {
+#'     message('Input value is not numeric. Return NULL.')
+#'     return(NULL)
+#'   }
+#' 
+#'   result <- convertBase(x = decVal,
+#'                         inputBase  = '10',
+#'                         outputBase = '2')
+#' 
+#'   return(result)
+#' }
+#' 
+#' #' Convert binary value to decimal value
+#' #'
+#' #' @export
+#' convertBin2Dec <- function(x) {
+#'   binVal <- assureNumeric(x)
+#' 
+#'   if (is.null(binVal)) {
+#'     message('Input value is not numeric. Return NULL.')
+#'     return(NULL)
+#'   }
+#' 
+#'   result <- convertBase(x = binVal,
+#'                         inputBase  = '2',
+#'                         outputBase = '10')
+#' 
+#'   return(result)
+#' }
